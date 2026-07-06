583ca2aa51525cae7af61c735cd2c44516f3cbe6
Prashanth M <Prashanthbgm@gmail.com>
2026-07-07 01:34:42 +0530

    string rev words

--- strings/str.py
+++ strings/str.py
@@ -1,12 +1,88 @@
-sentence=[["i love that gril very much"],["but she doesnt understand me"],["i also failed to understand her"]]
+# sentence=[["i love that gril very much"],["but she doesnt understand me"],["i also failed to understand her"]]
 
-for sentens in sentence:
-    maxword=0
-    comp_senc=" "
-    if len(sentens[0].split())>maxword:
-        maxword=len(sentens[0].split())
-        comp_senc=" ".join(sentens)
-print(maxword)
-print(comp_senc)
+# for sentens in sentence:
+#     maxword=0
+#     comp_senc=" "
+#     if len(sentens[0].split())>maxword:
+#         maxword=len(sentens[0].split())
+#         comp_senc=" ".join(sentens)
+# print(maxword)
+# print(comp_senc)
 
 
+# """find max unique string"""
+# sentence=[
+#     ["good","good","bad"],
+#     ["good","bad"],
+#     ["good"]
+#     ]
+
+# for ch in sentence:
+#     count=0
+#     max_count=0
+#     words=" "
+#     for char in set(ch):
+#         word=set()
+#         if char in word:
+#             count+=1
+#         word.add(char)
+#         if count>max_count:
+#             words=word
+#     print(max_count)
+#     print(words)
+"""Strings revisions"""
+"""reverse string"""
+# strs="Prashanth"
+
+# # print(strs[::-1])
+# left=0
+# right=len(strs)-1
+
+# while left<right:
+#     strs[left],strs[right]=strs[right],strs[left]
+#     left+=1
+#     right-=1
+"""reverse the words"""
+sentence="hi Prashanth M"
+
+# left=0
+# right=len(sentence)-1
+
+# while left<right:
+#     sentence[left],sentence[right]=sentence[right],sentence[left]
+#     left+=1
+#     right-=1
+
+# print(sentence[0])
+# words=sentence.split()
+
+# print(words)
+
+# w=" ".join(words[::-1]) 
+# print(w)
+
+result=""
+current_w=""
+
+for i in range(len(sentence)-1,-1,-1):
+    char=sentence[i]
+    # print(char,end="")
+
+    if char !=" ":
+        current_w=char+current_w
+        print(current_w)
+    else:
+        if current_w!="":
+            if result=="":
+                result=current_w
+            else:
+                result=result+" "+current_w
+        current_w=""
+
+   
+if current_w!="":
+    if result=="":
+        result=current_w
+    else:
+        result=result+" "+current_w
+print(result)
